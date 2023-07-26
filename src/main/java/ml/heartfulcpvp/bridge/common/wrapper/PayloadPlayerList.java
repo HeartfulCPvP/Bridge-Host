cb0d2fecc555e0170e012542c948da2dd2972176
Nodoka <78198198+Nodoka4318@users.noreply.github.com>
2023-07-26 15:44:03 +0900

    a

---
+++ src/main/java/ml/heartfulcpvp/bridge/common/wrapper/PayloadPlayerList.java
@@ -0,0 +1,34 @@
+package ml.heartfulcpvp.bridge.common.wrapper;
+
+import com.google.gson.Gson;
+
+/**
+ * 各サーバーのプレイヤー一覧を送るためのやつ
+ */
+public class PayloadPlayerList {
+    private String serverName;
+    private String[] players;
+
+    public PayloadPlayerList(String serverName, String[] players) {
+        this.serverName = serverName;
+        this.players = players;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public String[] getPlayers() {
+        return players;
+    }
+
+    public String toJsonStr() {
+        var gson = new Gson();
+        return gson.toJson(this);
+    }
+
+    public static PayloadVariables fromJsonStr(String jsonStr) {
+        var gson = new Gson();
+        return gson.fromJson(jsonStr, PayloadVariables.class);
+    }
+}
